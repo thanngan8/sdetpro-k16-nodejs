6a5a847b64e108fc5b320bfd6c3e42deec9f0ac5
thanngan88 <thanngan.tttn@gmail.com>
2025-01-09 21:13:50 +0700

    update lab_3-3.js

--- labs/lab-3/lab_3-3.js
+++ labs/lab-3/lab_3-3.js
@@ -1,19 +1,19 @@
 let intArr = [12, 34, 1, 16, 28];
 
 function sort(arr) {
-    for (let i = 0; i < intArr.length - 1; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         let minIndex = i;
 
         // Find index of the minimum element in the unsorted array
-        for (let j = i + 1; j < intArr.length; j++) {
-            if (intArr[j] < intArr[minIndex]) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
         // Swap the minimum element with the element at current index
-        let temp = intArr[i];
-        intArr[i] = intArr[minIndex];
-        intArr[minIndex] = temp;
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
 sort(intArr);
